566d3776c004726342fe21911bdce588c0cf7048
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2018-12-09 11:49:28 +0000

    Add public setters for ErrorNumber and ErrorMessage in RestResponseBase so that they can be populated when responses have DriverException set to null.

--- Shared Resources/Response Classes/RestResponseBase.cs
+++ Shared Resources/Response Classes/RestResponseBase.cs
@@ -8,26 +8,12 @@ namespace ASCOM.Remote
     public abstract class RestResponseBase
     {
         private Exception exception;
-        private int errorNumber = 0;
-        private string errorMessage = "";
 
         public int ClientTransactionID { get; set; }
         public int ServerTransactionID { get; set; }
         public string Method { get; set; }
-        public int ErrorNumber
-        {
-            get
-            {
-                return errorNumber;
-            }
-        }
-        public string ErrorMessage
-        {
-            get
-            {
-                return errorMessage;
-            }
-        }
+        public int ErrorNumber { get; set; } = 0;
+        public string ErrorMessage { get; set; } = "";
         public Exception DriverException
         {
             get
@@ -39,8 +25,8 @@ namespace ASCOM.Remote
                 exception = value;
                 if (exception != null)
                 {
-                    errorNumber = exception.HResult;
-                    errorMessage = exception.Message;
+                    ErrorNumber = exception.HResult;
+                    ErrorMessage = exception.Message;
                 }
             }
         }
