ad07c8bc494d62e6d2768d29e8f98f0577d50170
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2019-03-10 12:33:58 +0000

    Build process improvement Ensure that NuGet packages are refreshed before building so that first time command line builds will run as expected

--- BuildRemote.cmd
+++ BuildRemote.cmd
@@ -6,6 +6,9 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary
 SET RemoteDirectory=%cd%
 @echo Current directory: %RemoteDirectory%
 
+@echo Restoring packages
+msbuild /t:restore /p:configuration=Debug;Platform="Any CPU"
+
 @echo Building Debug AnyCPU in directory %cd%
 msbuild /t:rebuild /p:configuration=Debug;Platform="Any CPU"
 
